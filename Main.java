7c75fdf5da46d7a0d0364f50c894641bb1d7be9f
Tabassum-code <tabuj2706@gmail.com>
2024-10-13 22:59:29 +0530

    MultiCach

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main 
+{
+	public static void main(String[] args) 
+	{
+      try{
+		int x = sc.nextInt()
+		int y = Integer.parseInt(args[1]);
+
+		int z = x / y;
+		System.out.println("Result = " + z);
+	  }
+      catch(ArithmeticException ex){
+		  System.out.println("Exception: cannot divide a number by zero!");
+	  }
+	  catch(ArrayIndexOutOfBoundsException ex){
+		  System.out.println("Exception: please provide minimum 2 cmd args");
+	  }
+	  catch(NumberFormatException ex){
+		  System.out.println("Exception: please provide number without decimal point as args");
+	  }
+	  System.out.println("--- DONE ---");
+	}
+}
